88b0e559090cc45238a6f5c89dc368fb20bc0ce5
ohjinseo <ojs835@naver.com>
2022-02-16 16:56:44 +0900

    OAuth 테스트 : WebMVCTest 스캔 대상에서 SecurityConfig 제외

--- src/test/java/com/jin/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jin/springboot/web/HelloControllerTest.java
@@ -1,10 +1,13 @@
 package com.jin.springboot.web;
 
+import com.jin.springboot.config.auth.SecurityConfig;
 import com.jin.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.*;
@@ -14,7 +17,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 public class HelloControllerTest{
     @Autowired
     private MockMvc mvc;
