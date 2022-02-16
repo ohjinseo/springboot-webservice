b0513c62d522ec02b64f5068bb70c3906e06d4b8
ohjinseo <ojs835@naver.com>
2022-02-16 15:48:55 +0900

    네이버 OAuth2 등록

--- src/main/java/com/jin/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jin/springboot/config/auth/dto/OAuthAttributes.java
@@ -32,6 +32,9 @@ public class OAuthAttributes {
 
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -43,6 +46,18 @@ public class OAuthAttributes {
                 .picture((String)attributes.get("picture"))
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName).build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     //처음 가입 시 엔티티 생성 -> 이후 SessionUser 생성
