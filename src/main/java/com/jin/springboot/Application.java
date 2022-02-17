156e78170c716cf3d6fefa800de35135f3640e07
ohjinseo <ojs835@naver.com>
2022-02-17 20:31:02 +0900

    RDS 환경 profile 설정

--- src/main/java/com/jin/springboot/Application.java
+++ src/main/java/com/jin/springboot/Application.java
@@ -3,7 +3,7 @@ package com.jin.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+//2
 @SpringBootApplication
 //@EnableJpaAuditing  -> WebMvcTest가 JPA 관련 Bean들을 로드하지 않기때문에 오류 발생하여 삭제
 public class Application {
